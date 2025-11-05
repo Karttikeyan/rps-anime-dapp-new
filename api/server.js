03651d4cf22f88103cc1d2ad3e57e4b96191a62b
Karttikeyan <nokierounick@gmail.com>
2025-11-05 04:57:19 +0100

    Add Node.js server for React Router

--- api/server.js
+++ api/server.js
@@ -1,5 +1,5 @@
 const { createRequestHandler } = require('@react-router/node');
 
-module.exports = createRequestHandler(
-  './build/server/index.js'
-);
+module.exports = createRequestHandler({
+  build: require('./build/server/index.js')
+});
